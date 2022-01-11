bf8c1e2f4d0ebf8f096833660f078d06e2a10fba
Tomoyuki Shimizu <tomoyuki-shimizu@cybozu.co.jp>
2022-01-11 11:18:03 +0900

    Update script for making vimrc

--- make_vimrc.sh
+++ make_vimrc.sh
@@ -4,14 +4,6 @@ echo '" This .vimrc are produced from https://github.com/simics-ja/my-vim-config
 echo '" Install plugins\n' >> .vimrc
 cat ./init/plug_install.vim >> .vimrc
 
-echo '" Plugin configs\n' >> .vimrc
-
-find ./init/plugins/ -type f -name *.init.vim | xargs cat >> .vimrc
-
-echo '" Plugin keymap configs\n' >> .vimrc
-
-find ./init/plugins/ -type f -name *.keymap.vim | xargs cat >> .vimrc
-
 echo '" Main configs\n' >> .vimrc
 
 cat ./init/main.init.vim >> .vimrc
@@ -19,6 +11,14 @@ cat ./init/main.init.vim >> .vimrc
 echo '" Main keymap configs\n' >> .vimrc
 
 cat ./init/main.keymap.vim >> .vimrc
+
+echo '" Plugin configs\n' >> .vimrc
+
+find ./init/plugins/ -type f -name *.init.vim | xargs cat >> .vimrc
+
+echo '" Plugin keymap configs\n' >> .vimrc
+
+find ./init/plugins/ -type f -name *.keymap.vim | xargs cat >> .vimrc
 
 sed -i -e "s/^\(Plug 'antoinemadec\/FixCursorHold.nvim'.*\)/\" \1/g" .vimrc
 # The sed on macOS makes a backup file with '-e' suffix added.
